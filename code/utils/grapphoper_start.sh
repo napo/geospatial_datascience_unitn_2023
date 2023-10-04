4dcfd705a5cb888833bdd0953cf15a8253490484
napo <napo@fbk.eu>
2023-10-04 14:28:10 +0200

    update

--- code/utils/grapphoper_start.sh
+++ code/utils/grapphoper_start.sh
@@ -1,7 +1,7 @@
  if [ -d graph-cache/ ]; 
  then 
-	wget -O https://osmit-estratti.wmcloud.org/dati/poly/comuni/pbf/022205_Trento.osm.pbf;
- 	wget -O https://repo1.maven.org/maven2/com/graphhopper/graphhopper-web/7.0/graphhopper-web-7.0.jar;
-	wget -O https://raw.githubusercontent.com/napo/geospatial_datascience_unitn_2023/main/data/conf_grapphoper/config_trento.yml;
+	wget -c https://osmit-estratti.wmcloud.org/dati/poly/comuni/pbf/022205_Trento.osm.pbf;
+ 	wget -c https://repo1.maven.org/maven2/com/graphhopper/graphhopper-web/7.0/graphhopper-web-7.0.jar;
+	wget -c https://raw.githubusercontent.com/napo/geospatial_datascience_unitn_2023/main/data/conf_grapphoper/config_trento.yml;
  fi
 java -jar graphhopper*.jar server config_trento.yml
